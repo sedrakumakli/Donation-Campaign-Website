01ee473dd759239f97e34c9f41c6879846e48418
Batoulabdulhadi <batoulabdulhadi1@gmail.com>
2026-08-29 16:58:07 +0300

    mainEdits

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 // import { CacheProvider } from '@emotion/react';
 import { BrowserRouter } from "react-router-dom";
-// import rtlCache from './theme/rtl';
+import rtlCache from './theme/rtl';
 import theme from "./theme/theme";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { CacheProvider } from "@emotion/react";
 
 document.documentElement.setAttribute("dir", "rtl");
 document.documentElement.setAttribute("lang", "ar");
@@ -18,7 +19,7 @@ const queryClient = new QueryClient({
 });
 
 createRoot(document.getElementById("root")).render(
-  // <CacheProvider value={rtlCache}>
+  <CacheProvider value={rtlCache}>
   <ThemeProvider theme={theme}>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
@@ -27,5 +28,5 @@ createRoot(document.getElementById("root")).render(
       </BrowserRouter>
     </QueryClientProvider>
   </ThemeProvider>,
-  // </CacheProvider>,
+   </CacheProvider>,
 );
